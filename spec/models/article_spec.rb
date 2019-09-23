4b77bec3da16b0c818b9af257a4001bf0583df95
luca <l.lobacher@web.de>
2019-09-23 22:09:55 +0200

    Creates article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,15 +5,16 @@ RSpec.describe Article, type: :model do
     it {is_expected.to have_db_column :id}
     it {is_expected.to have_db_column :title}
     it {is_expected.to have_db_column :content}
+  end
 
-    describe 'Validates attributes' do
-      it {is_expected.to vaildate_presence_of :title}
-      it {is_expected.to vaildate_presence_of :content}
-    end
+  describe 'Validates attributes' do
+    it {is_expected.to vaildate_presence_of :title}
+    it {is_expected.to vaildate_presence_of :content}
+  end
 
-    describe 'Factory' do
-      it 'should have a valid Factory' do
-        expect(FactoryBot.create(:article)).to be_valid
-      end
+  describe 'Factory' do
+    it 'should have a valid Factory' do
+      expect(FactoryBot.create(:article)).to be_valid
     end
+  end
 end
