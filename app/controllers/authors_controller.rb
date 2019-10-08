7407c89fc136aa81cbae8619c226da5c1fdb9c1e
luca <l.lobacher@web.de>
2019-10-08 23:14:58 +0200

    Adds new and create action to authors controller and form to new html

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,21 @@ class AuthorsController < ApplicationController
   def index
     @author = Author.all
   end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+
+    @author.save
+    redirect_to @author
+  end
+
+  private
+  def author_params
+    params.require(:author).permit(:name, :genre)
+  end
+  
 end
